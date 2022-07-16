269137eb873b90971c69966084bc393a01b2e55b
macilaci91 <attila.farkasinszki@gmail.com>
2022-07-16 18:34:14 +0200

    Add multiplication table task

---
+++ multiplicationTable/multiplicationTable/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace multiplicationTable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            Random random = new Random();
+
+            string fromUser = Console.ReadLine();
+
+            if (fromUser != "")
+            {
+                number = Convert.ToInt32(fromUser);
+            } else
+            {
+                number = random.Next(100);
+            }
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine("{0} * {1} = {2}",number,i,number * i);
+            }
+            Console.ReadKey();
+        }
+    }
+}
